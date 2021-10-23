c395cdf5b7df4340f85cd9383889f2621e3e1ffe
Palmer Cox <palmer@fiscalnote.com>
2021-10-23 17:42:00 -0400

    Reduce public API surface of Buffer
    * Mark the from_slice() and from_polynum() methods as pub(crate)
    * Remove append() method since its not used in the crate and it doesn't
      need to be part of the public API

--- src/buffer.rs
+++ src/buffer.rs
@@ -2,14 +2,6 @@ use ::gf::poly::Polynom;
 use core::ops::{Deref, DerefMut};
 
 /// Buffer for block encoded data
-/// # Example
-/// ```rust
-/// use reed_solomon::Buffer;
-///
-/// let buffer = Buffer::from_slice(&[1, 2, 3, 4], 2);
-/// assert_eq!(&[1, 2], buffer.data());
-/// assert_eq!(&[3, 4], buffer.ecc());
-/// ```
 #[derive(Debug, Copy, Clone)]
 pub struct Buffer {
     poly: Polynom,
@@ -18,7 +10,7 @@ pub struct Buffer {
 
 impl Buffer {
     /// Create buffer from internal polynom
-    pub fn from_polynom(poly: Polynom, data_len: usize) -> Self {
+    pub(crate) fn from_polynom(poly: Polynom, data_len: usize) -> Self {
         Buffer {
             poly: poly,
             data_len: data_len,
@@ -26,7 +18,7 @@ impl Buffer {
     }
 
     /// Create buffer from [u8] slice
-    pub fn from_slice(slice: &[u8], data_len: usize) -> Self {
+    pub(crate) fn from_slice(slice: &[u8], data_len: usize) -> Self {
         Buffer {
             poly: Polynom::from(slice),
             data_len: data_len,
@@ -41,15 +33,6 @@ impl Buffer {
     /// Slice with error correction core of encoced block
     pub fn ecc(&self) -> &[u8] {
         &self[self.data_len..]
-    }
-
-    /// Add byte string to the end of buffer
-    pub fn append(&mut self, rhs: &[u8]) {
-        let ofst = self.len();
-        self.set_length(ofst + rhs.len());
-        for (i, rhs_x) in rhs.iter().enumerate() {
-            self[i + ofst] = *rhs_x;
-        }
     }
 }
 
